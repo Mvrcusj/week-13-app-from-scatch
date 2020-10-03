b66bd1a9383f07d3dc37317848b8b89d220ca5c5
mvrcusj <mvrcusj@gmail.com>
2020-10-02 20:02:40 -0400

    Full front end shopping cart app

--- shopping-cart/components/Home.js
+++ shopping-cart/components/Home.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { addToCart } from './actions/cartActions'
 
 class Home extends Component{
+
+    handleClick = (id)=>{
+        this.props.addToCart(id);
+    }
+
     render(){
         let itemList = this.props.items.map(item=>{
             return(
@@ -11,6 +17,35 @@ class Home extends Component{
                         <span className="card-title">{item.title}</span>
                         <span to="/" className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></span>
                     </div>
+
+                    <div className="card-content">
+                        <p>{item.desc}</p>
+                        <p><b>Price: {item.price}$</b></p>
+                    </div>
                 </div>
             )
-export default connect(mapStateToProps)(Home)
+        })
+        return(
+            <div className="container">
+                <h3 className="center">Our items</h3>
+                <div className="box">
+                    {itemList}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return {
+        items: state.items
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- shopping-cart/components/reducers/cartReducer.js
+++ shopping-cart/components/reducers/cartReducer.js
@@ -1,10 +1,11 @@
+import { bindActionCreators } from 'redux'
 import Item1 from '../../images/item1.jpg'
 import Item2 from '../../images/item2.jpg'
 import Item3 from '../../images/item3.jpg'
 import Item4 from '../../images/item4.jpg'
 import Item5 from '../../images/item5.jpg'
 import Item6 from '../../images/item6.jpg'
-
+import { ADD_TO_CART, REMOVE_ITEM, SUB_QUANTITY, ADD_QUANTITY } from '../actions/action-types/cart-actions'
 
 const initState = {
     items: [
@@ -21,7 +22,77 @@ const initState = {
 }
 const cartReducer= (state = initState,action)=>{
     
-    return state;
+    if(action.type === ADD_TO_CART){
+        let addedItem = state.items.find(item => item.id === action.id)
+        let existed_item = state.addedItems.find(item => action.id === item.id)
 
-}
+        if(existed_item) {
+            addedItem.quantity += 1
+            return{
+                ...state,
+                total: state.total + addedItem.price
+            }
+        }
+            else{
+                addedItem.quantity = 1;
+
+                let newTotal = state.total + addedItem.price
+                return{
+                    ...state,
+                    addedItems: [...state.addedItems, addedItem],
+                    total: newTotal
+                }
+            }
+        }
+
+        if(action.type === REMOVE_ITEM) {
+            let itemToRemove = state.addedItems.find(item => action.id === item.id)
+            let new_items = state.addedItems.filter(item => action.id !== item.id)
+
+            let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity)
+            console.log(itemToRemove)
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+
+        if(action.type === ADD_QUANTITY) {
+            let addedItem = state.items.find(item => item.id === action.id)
+                addedItem.quantity += 1
+                let newTotal = state.total + addedItem.price
+                
+                return{
+                    ...state,
+                    total: newTotal
+                }
+        }
+
+        if(action.type === SUB_QUANTITY) {
+            let addedItem = state.items.find(item => item.id === action.id)
+
+            if (addedItem.quantity === 1) {
+                let new_items = state.addedItems.filter(item => item.id !== action.id)
+                let newTotal = state.total - addedItem.price
+
+                return{
+                    ...state,
+                    addedItems: new_items,
+                    total: newTotal
+                }
+            }
+            else {
+                addedItem.quantity -= 1
+                let newTotal = state.total - addedItem.price
+
+                return{
+                    ...state,
+                    total: newTotal
+                }
+            }
+        }
+        return state
+    }
+
 export default cartReducer;
